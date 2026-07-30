980da90da65f3528ef72910a900a1052062aa642
Elizabeth <eli2908>
2026-07-29 21:21:36 -0600

    ajustesMiddleware

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,32 @@
+// module.exports = [
+//   'strapi::logger',
+//   'strapi::errors',
+//   'strapi::security',
+//   'strapi::cors',
+//   'strapi::poweredBy',
+//   'strapi::query',
+//   'strapi::body',
+//   'strapi::session',
+//   'strapi::favicon',
+//   'strapi::public',
+// ];
+
 module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::logger",
+  "strapi::errors",
+  "strapi::security",
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  {
+    name: "strapi::session",
+    config: {
+      cookie: {
+        secure: false,
+      },
+    },
+  },
+  "strapi::favicon",
+  "strapi::public",
 ];
