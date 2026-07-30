c2c18a5ba714ae63ec194a6caf5f19109c126fd3
Elizabeth <eli2908>
2026-07-29 21:26:36 -0600

    ajustesServer2

--- config/server.js
+++ config/server.js
@@ -13,6 +13,8 @@ module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
 
+  url: env("PUBLIC_URL", "https://backendstrapi-k33y.onrender.com"),
+
   proxy: true,
 
   app: {
